05a35f95102b4e016fa09690db550b9cf0ca681c
vessnx <vessnx@outlook.com>
2018-03-10 11:40:31 +0800

    配置vue

---
+++ frontend/vue.config.js
@@ -0,0 +1,24 @@
+// vue.config.js
+module.exports = {
+    chainWebpack: chainableConfig => {
+        // modify config with webpack-chain
+        // https://github.com/mozilla-neutrino/webpack-chain
+    },
+
+    configureWebpack: config => {
+        // mutate config directly, or return new config
+    },
+
+    // object literal will be merged into base config using webpack-merge
+    configureWebpack: {
+        output: {
+            publicPath: './'
+        }
+    },
+    devServer: {
+        port: 8080,
+        proxy: {
+
+        },
+    }
+};
