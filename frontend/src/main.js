936070aa57fb62e5391578069fa3d5a5fbe5971b
vessnx <vessnx@outlook.com>
2018-03-27 19:34:56 +0800

    Revert "element.ui"
    This reverts commit e10e8d89263c1879ca11a94c8dd7715502114382.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
-Vue.use(ElementUI);
+Vue.use(iView);
 
 Vue.config.productionTip = false
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,18 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: { // 状态
-    currentPage:null,
+  state: {
+
   },
-  mutations: { // 状态改变记录
-      ['changeCurrentPage'](state, currentPage){
-          state.currentPage = currentPage;
-      }
+  mutations: {
+
   },
-  actions: { // 动作
-      changeCurrentPage({commit}, currentPage){
-          commit('changeCurrentPage', currentPage);
-      }
+  actions: {
 
   }
 })
